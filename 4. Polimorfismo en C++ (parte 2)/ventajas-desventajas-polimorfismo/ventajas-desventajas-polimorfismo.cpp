49389c7763376cbb960d06146dec4bd901d906eb
Eliezer López <contacto@eliezerlopez.com>
2022-05-31 07:12:31 +0200

    Ejercicios capítulo 4

---
+++ 4. Polimorfismo en C++ (parte 2)/ventajas-desventajas-polimorfismo/ventajas-desventajas-polimorfismo.cpp
@@ -0,0 +1,77 @@
+/*
+ * Curso: C++ Avanzado 2
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Empleado {
+public:
+    Empleado(string _nombre, string _primerApellido) : nombre(_nombre), primerApellido(_primerApellido){};
+    virtual void mostrarInformacion() const = 0;
+    // virtual void mostrarInformacion() const;
+protected:
+    string nombre, primerApellido;
+};
+
+class Supervisor : public Empleado {
+public:
+    Supervisor(string _nombre, string _primerApellido, int _idEquipo) : Empleado(_nombre, _primerApellido){
+        idEquipo = _idEquipo;
+    };
+    void mostrarInformacion() const;
+private:
+    int idEquipo;
+};
+
+void Supervisor::mostrarInformacion() const{
+    cout << "El Supervisor " << nombre << " " << primerApellido << " gestiona el equipo número " << idEquipo << "." << endl;
+}
+
+class Director : public Empleado {
+public:
+    Director(string _nombre, string _primerApellido, int _idEquipo, float _presupuestoAnual): Empleado(_nombre, _primerApellido){
+        idEquipo = _idEquipo;
+        presupuestoAnual = _presupuestoAnual;
+    };
+    void mostrarInformacion() const;
+private:
+    int idEquipo;
+    float presupuestoAnual;
+};
+
+void Director::mostrarInformacion() const{
+    cout << "El Director " << nombre << " " << primerApellido << " dispone de un presupuesto de " << presupuestoAnual <<  " euros para gestionar el equipo número " << idEquipo << "." << endl;
+}
+
+int main(){
+
+    // Empleado *supervisor = new Supervisor("Dwight", "Schrute", 18);
+    // Empleado *director = new Director("Michael", "Scott", 1, 90500.30);
+
+    // supervisor->mostrarInformacion();
+    // director->mostrarInformacion();
+
+    vector<Empleado*> cargosDeGestion;
+
+    cargosDeGestion.push_back(new Supervisor("Dwight", "Schrute", 18));
+    cargosDeGestion.push_back(new Director("Michael", "Scott", 1, 90500.30));
+    cargosDeGestion.push_back(new Supervisor("Jim", "Halpert", 3));
+    cargosDeGestion.push_back(new Director("Pam", "Beesly", 30, 10300.46));
+
+    for(int i=0; i<cargosDeGestion.size(); i++){
+        cargosDeGestion.at(i)->mostrarInformacion();
+    }
+
+    return 0;
+};
+
+/*
+void Empleado::mostrarInformacion() const{
+    cout << "El empleado se llama " << nombre << " " << primerApellido << endl;
+}
+*/
